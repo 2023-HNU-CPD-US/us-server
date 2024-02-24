1ca2ef0a6bed50c71bc0cb2860c823c171208708
dlwlstn3585 <42939148+dlwlstn3585@users.noreply.github.com>
2024-02-24 23:32:42 +0900

    Update main.py
    대충 키 값이였던 것

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import openai
-openai.api_key = "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"
+openai.api_key = "대충 키 값이였던 것"
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
